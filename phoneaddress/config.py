4cb316c8547670be287eba8ae48e455518c4cb4f
Andrey Kulenko <ankubel904@gmail.com>
2025-05-19 00:23:50 +0300

    Убрать лишний экспорт `server_config`

--- phoneaddress/config.py
+++ phoneaddress/config.py
@@ -3,7 +3,7 @@ import os
 from uvicorn import Config
 
 
-__all__ = ["REDIS_URL", "HOST", "PORT", "server_config"]
+__all__ = ["REDIS_URL", "HOST", "PORT"]
 
 REDIS_URL = os.getenv("PHONEADDRESS_REDIS_URL", "redis://localhost:6379")
 HOST = os.getenv("PHONEADDRESS_HOST", "0.0.0.0")
