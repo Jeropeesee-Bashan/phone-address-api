2fe798a3f32b02b31633f998fbe3a7e106f42e35
Andrey Kulenko <ankubel904@gmail.com>
2025-05-19 00:42:14 +0300

    Добавить примеров для поля `phone`

--- phoneaddress/schemas.py
+++ phoneaddress/schemas.py
@@ -1,9 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-from pydantic_extra_types.phone_numbers import PhoneNumber, PhoneNumberValidator
+from pydantic_extra_types.phone_numbers import (
+    PhoneNumber,
+    PhoneNumberValidator,
+)
 from typing import Annotated
 
 
 class PhoneAddressData(BaseModel):
-    phone: Annotated[PhoneNumber, PhoneNumberValidator("RU", "E164")]
+    phone: Annotated[PhoneNumber, PhoneNumberValidator("RU", "E164")] = Field(
+        examples=["+79991234567", "+7 900 111 11 11", "900 222 22 22"]
+    )
     address: str
